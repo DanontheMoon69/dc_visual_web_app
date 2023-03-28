0d481fa84eea452a99da9803a17969b132122e00
DanontheMoon69 <danaguilera87@gmail.com>
2023-03-27 22:21:51 -0500

    env

---
+++ pages/Apparel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Apparel = () => {
+  return (
+    <div className=" w-full h-4/5 pt-24 flex justify-center items-center">
+      <iframe
+        src="https://dc-visual.printify.me/"
+        className="w-full h-96"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Apparel;
