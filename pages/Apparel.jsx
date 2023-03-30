674d3a388c5eaeda13557fa1c261f7205497a39c
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-03-30 10:31:07 -0500

    Update Apparel.jsx

--- pages/Apparel.jsx
+++ pages/Apparel.jsx
@@ -5,7 +5,7 @@ const Apparel = () => {
     <div className=" w-full h-4/5 pt-24 flex justify-center items-center">
       <iframe
         src="https://dc-visual.printify.me/"
-        className="w-full h-96"
+        className="w-full h-[600px]"
       ></iframe>
     </div>
   );
