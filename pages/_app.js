16c45f6aed1de4b44fe1e3fdc1e77bf18b14810b
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-03-30 21:19:03 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import { Layout } from "@/components";
 import "@/styles/globals.css";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
-    <GoogleAnalytics
-      gaMeasurementId={NEXT_PUBLIC_GA_MEASUREMENT_ID}
-      trackPageViews
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </GoogleAnalytics>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
