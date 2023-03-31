c164061b3eceba290758a8852eb6c09580826298
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-03-30 21:34:29 -0500

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -70,7 +70,7 @@ const Header = () => {
               Get a Quote
             </div>
           </Link>
-          <a href="tel:+7149306037">
+          <a href="tel:+17149306037">
             <div className=" bg-gray-500 rounded-full text-xs sm:text-xs md:text-sm px-4 py-2 text-center tracking-wide hover:ease-in transform hover:scale-105 hover:transition hover:duration-100 hover:shadow-lg">
               Give us a Call
             </div>
