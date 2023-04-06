71d78308b3d8afbb38db8dca4e163044c4fd8593
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-04-05 20:36:18 -0500

    Add files via upload

--- pages/FAQ.jsx
+++ pages/FAQ.jsx
@@ -15,14 +15,14 @@ const FAQ = () => {
       </Head>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
       <Script id="ga-script" strategy="lazyOnload">
         {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
       page_path: window.location.pathname,
     });
         `}
